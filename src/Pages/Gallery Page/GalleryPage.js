fecdf79d6d4474018c2ecdd1377b5616dfd49064
dreayoung <youngmandrea@gmail.com>
2022-02-24 22:36:45 -0500

    final

--- src/Pages/Gallery Page/GalleryPage.js
+++ src/Pages/Gallery Page/GalleryPage.js
@@ -62,39 +62,66 @@ export default function Gallery() {
       </div>
 
       <div className="grid grid-cols-2 gap-10 ml-2 mt-10 px-12 justify-center md:ml-44 lg:mx-auto">
-        <ScrollLink
+      <ScrollLink
           activeClass="active"
-          to="HTA_x_HoodstyleHub"
+          to="the_office"
           spy={true}
           smooth={true}
         >
-          {/*  */}
-          <div className="deg_0 relative w-32 h-32 rounded-full hover:animate-pulse">
-            <div className="w-32 h-32 rounded-full absolute lg opacity-40 cursor-pointer lg:w-44 lg:h-44" />
+          <div className="deg_0 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
+            <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
+            <img
+              src={
+                process.env.PUBLIC_URL + '/highlightimagesforgallerypage/B1.jpg'
+              }
+              alt="the office photoshoot"
+              className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
+            />
+          </div>
+        </ScrollLink>
+
+      <ScrollLink activeClass="active" to="tiiied" spy={true} smooth={true}>
+          <div className="deg-45 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
+            <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
             <img
               src={
                 process.env.PUBLIC_URL +
-                '/highlightimagesforgallerypage/30C7F533-E7C2-4042-A20A-0007CEA41EFA.jpg'
+                '/highlightimagesforgallerypage/Tiiied.jpg'
               }
-              alt="coor"
-              className="rounded-full ring ring-gray-200 w-32 h-32 lg:w-44 lg:h-44"
+              alt="tiiied photoshoot"
+              className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
             />
           </div>
         </ScrollLink>
 
         <ScrollLink activeClass="active" to="smgo" spy={true} smooth={true}>
-          <div className="deg-45 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
+          <div className="deg-90 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
             <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
             <img
               src={
                 process.env.PUBLIC_URL +
                 '/highlightimagesforgallerypage/Show Must Go On (1).jpg'
               }
-              alt="photoshoot"
+              alt="show must go on photoshoot"
               className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
             />
           </div>
         </ScrollLink>
+
+      <ScrollLink activeClass="active" to="yas" spy={true} smooth={true}>
+          <div className="deg-135 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
+            <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                '/highlightimagesforgallerypage/D072EF99-B5C5-4719-9A9F-72F166304576.JPG'
+              }
+              alt="yas apothecary photoshoot"
+              className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
+            />
+          </div>
+        </ScrollLink>
+
 
         <ScrollLink
           activeClass="active"
@@ -102,7 +129,7 @@ export default function Gallery() {
           spy={true}
           smooth={true}
         >
-          <div className="deg-90 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
+          <div className="deg-180 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
             <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
             <img
               src={
@@ -115,70 +142,20 @@ export default function Gallery() {
           </div>
         </ScrollLink>
 
-        <ScrollLink
-          activeClass="active"
-          to="dominatrix"
-          spy={true}
-          smooth={true}
-        >
-          <div className="deg-135 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
-            <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                '/highlightimagesforgallerypage/dominatrix.jpg'
-              }
-              alt="dominatrix photoshoot"
-              className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
-            />
-          </div>
-        </ScrollLink>
-
-        <ScrollLink activeClass="active" to="overtime" spy={true} smooth={true}>
-          <div className="deg-180 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
-            <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                '/highlightimagesforgallerypage/OT-2.jpg'
-              }
-              alt="colors photoshoot"
-              className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
-            />
-          </div>
-        </ScrollLink>
-
-        <ScrollLink activeClass="active" to="tiied" spy={true} smooth={true}>
-          <div className="deg-225 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
-            <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
-            <img
-              src={
-                process.env.PUBLIC_URL +
-                '/highlightimagesforgallerypage/Tiiied.jpg'
-              }
-              alt="tiied photoshoot"
-              className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
-            />
-          </div>
-        </ScrollLink>
-
-        <ScrollLink
-          activeClass="active"
-          to="the_office"
-          spy={true}
-          smooth={true}
-        >
+        <ScrollLink activeClass="active" to="grounded" spy={true} smooth={true}>
           <div className="deg-270 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
             <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
             <img
               src={
-                process.env.PUBLIC_URL + '/highlightimagesforgallerypage/B1.jpg'
+                process.env.PUBLIC_URL +
+                '/highlightimagesforgallerypage/Grounded (2).jpg'
               }
-              alt="the office photoshoot"
+              alt="grounded photoshoot"
               className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
             />
           </div>
         </ScrollLink>
+
 
         <ScrollLink className="active" to="colors" spy={true} smooth={true}>
           <div className="deg-315 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
@@ -190,6 +167,25 @@ export default function Gallery() {
               }
               alt="colors photoshoot"
               className="rounded-full ring ring-gray-200 w-32 h-32 lg:w-44 lg:h-44"
+            />
+          </div>
+        </ScrollLink>
+
+        <ScrollLink
+          activeClass="active"
+          to="dominatrix"
+          spy={true}
+          smooth={true}
+        >
+          <div className="deg-225 w-32 h-32  lg:w-44 lg:h-44 rounded-full hover:animate-pulse">
+            <div className="w-32 h-32  lg:w-44 lg:h-44 rounded-full absolute opacity-40 cursor-pointer" />
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                '/highlightimagesforgallerypage/dominatrix.jpg'
+              }
+              alt="dominatrix photoshoot"
+              className="rounded-full ring ring-gray-200 w-32 h-32  lg:w-44 lg:h-44"
             />
           </div>
         </ScrollLink>
@@ -233,7 +229,7 @@ export default function Gallery() {
       />
       <Slider
         imgs={GalleryPageImages.tiied}
-        shoot="Tiied"
+        shoot="Tiiied"
         id_name="tiied"
         photographer={['moiiimagg', 'digitalsbyzac']}
         styling={['ice.coleyy']}
@@ -284,6 +280,7 @@ export default function Gallery() {
       />
       <Slider
         imgs={GalleryPageImages.grounded}
+        id_name="grounded"
         shoot="Grounded"
         photographer={['crzywthacanon']}
         creative={['ice.coleyy']}
@@ -291,6 +288,7 @@ export default function Gallery() {
       />
       <Slider
         imgs={GalleryPageImages.yasapothecary}
+        id_name="yas"
         shoot="Yasapothecary"
         photographer={['curatorstun']}
         creative={['yasapothecary']}
