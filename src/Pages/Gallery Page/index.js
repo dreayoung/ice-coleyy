687e9b379df96a1fba5125d2d1987595ce99243e
dreayoung <youngmandrea@gmail.com>
2022-03-10 02:43:23 -0500

    added new

--- src/Pages/Gallery Page/index.js
+++ src/Pages/Gallery Page/index.js
@@ -12,17 +12,17 @@ const GalleryPageImages = {
     require('./Images/the_office/E0.jpg'),
   ],
   tiied: [
-    require('./Images/Tiiied/Tiiied (1).JPG'),
-    require('./Images/Tiiied/Tiiied (2).JPG'),
-    require('./Images/Tiiied/Tiiied (3).JPG'),
-    require('./Images/Tiiied/Tiiied (4).JPG'),
-    require('./Images/Tiiied/Tiiied (5).JPG'),
-    require('./Images/Tiiied/Tiiied (6).JPG'),
-    require('./Images/Tiiied/Tiiied (7).JPG'),
-    require('./Images/Tiiied/Tiiied (8).JPG'),
-    require('./Images/Tiiied/Tiiied (10).JPG'),
-    require('./Images/Tiiied/Tiiied (11).JPG'),
-    require('./Images/Tiiied/Tiiied (12).JPG'),
+    require('./Images/Tiiied/Tiiied (1).jpg'),
+    require('./Images/Tiiied/Tiiied (2).jpg'),
+    require('./Images/Tiiied/Tiiied (3).jpg'),
+    require('./Images/Tiiied/Tiiied (4).jpg'),
+    require('./Images/Tiiied/Tiiied (5).jpg'),
+    require('./Images/Tiiied/Tiiied (6).jpg'),
+    require('./Images/Tiiied/Tiiied (7).jpg'),
+    require('./Images/Tiiied/Tiiied (8).jpg'),
+    require('./Images/Tiiied/Tiiied (10).jpg'),
+    require('./Images/Tiiied/Tiiied (11).jpg'),
+    require('./Images/Tiiied/Tiiied (12).jpg'),
   ],
   overtime: [
     require('./Images/OvertimeFits/OT (2).jpg'),
@@ -40,11 +40,11 @@ const GalleryPageImages = {
     require('./Images/no_remorse/no remorse (8).jpg'),
   ],
   dominatrix: [
-    require('./Images/dominatrix/nr1.JPG'),
-    require('./Images/dominatrix/nr2.JPG'),
-    require('./Images/dominatrix/nr3.JPG'),
-    require('./Images/dominatrix/nr4.JPG'),
-    require('./Images/dominatrix/nr5.JPG'),
+    require('./Images/dominatrix/nr1.jpg'),
+    require('./Images/dominatrix/nr2.jpg'),
+    require('./Images/dominatrix/nr3.jpg'),
+    require('./Images/dominatrix/nr4.jpg'),
+    require('./Images/dominatrix/nr5.jpg'),
   ],
   colors: [
     require('./Images/ColorsProject_Indigo/Colors-Indigo-Cole-4.png'),
@@ -53,8 +53,8 @@ const GalleryPageImages = {
     require('./Images/ColorsProject_Indigo/Colors-Indigo-Cole-23.png'),
   ],
   colorsOfLux: [
-    require('./Images/ColorsOfLux/headshot1.PNG'),
-    require('./Images/ColorsOfLux/headshot2.PNG'),
+require('./Images/ColorsOfLux/headshot1.png'),
+require('./Images/ColorsOfLux/headshot2.png')
   ],
   show_must_go_on: [
     require('./Images/ShowMustGoOn/Smgo1.jpg'),
@@ -79,21 +79,22 @@ const GalleryPageImages = {
     require('./Images/Cage/Cage (1).jpg'),
     require('./Images/Cage/Cage (2).jpg'),
     require('./Images/Cage/Cage (3).jpg'),
-    require('./Images/Cage/Cage (4).jpg'),
+    require('./Images/Cage/Cage (4).jpg')
   ],
   yasapothecary: [
     require('./Images/Yasapothecary/27B0A6F3-C602-489D-BBDD-704DF3735DD4.JPG'),
     require('./Images/Yasapothecary/683D448C-6D32-4AB5-9C0C-9B754102C70B.JPG'),
     require('./Images/Yasapothecary/21605079-C338-47F2-8E5C-F746D16E7FD7.JPG'),
     require('./Images/Yasapothecary/D2E40559-1946-40FD-B8E8-A2A6BB84F7D4.JPG'),
-    require('./Images/Yasapothecary/D072EF99-B5C5-4719-9A9F-72F166304576.JPG'),
+    require('./Images/Yasapothecary/D072EF99-B5C5-4719-9A9F-72F166304576.JPG')
   ],
   fresh2death: [
     require('./Images/Fresh2Death by Fourthebr4nd/3CD327C1-6A4A-4671-BB1A-43410BF8B565.JPG'),
     require('./Images/Fresh2Death by Fourthebr4nd/AfterlightImage 2.jpeg'),
     require('./Images/Fresh2Death by Fourthebr4nd/AfterlightImage.jpeg'),
-    require('./Images/Fresh2Death by Fourthebr4nd/D8D231D7-FFD2-4640-9DF7-C1A663685613.JPG'),
+    require('./Images/Fresh2Death by Fourthebr4nd/D8D231D7-FFD2-4640-9DF7-C1A663685613.JPG')
   ],
+
 };
 
 export default GalleryPageImages;
