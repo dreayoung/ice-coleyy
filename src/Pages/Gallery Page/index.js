e3617bbf23b74428ff02a2677339504f37244d21
dreayoung <youngmandrea@gmail.com>
2022-03-11 10:04:04 -0500

    moving files to pub

--- src/Pages/Gallery Page/index.js
+++ src/Pages/Gallery Page/index.js
@@ -1,99 +1,120 @@
 const GalleryPageImages = {
   hta_hoodstyle: [
-    require('./Images/HTA_x_HoodstyleHub_photoshoot/HTA (1).jpg'),
-    require('./Images/HTA_x_HoodstyleHub_photoshoot/HTA (3).jpg'),
-    require('./Images/HTA_x_HoodstyleHub_photoshoot/HTA (4).jpg'),
-    require('./Images/HTA_x_HoodstyleHub_photoshoot/HTA (5).jpg'),
+    process.env.PUBLIC_URL +
+      '/Images/HTA_x_HoodstyleHub_photoshoot/HTA (1).jpg',
+    process.env.PUBLIC_URL +
+      '/Images/HTA_x_HoodstyleHub_photoshoot/HTA (3).jpg',
+    process.env.PUBLIC_URL +
+      '/Images/HTA_x_HoodstyleHub_photoshoot/HTA (4).jpg',
+    process.env.PUBLIC_URL +
+      '/Images/HTA_x_HoodstyleHub_photoshoot/HTA (5).jpg',
   ],
   the_office: [
-    require('./Images/The Office/5E89C17A-C97C-4ABC-B383-5BEB423550FB.JPG'),
-    require('./Images/The Office/B15E082D-C93A-47D2-A96D-D9E6C4E62294.JPG'),
-    require('./Images/The Office/DDF2B544-5DBE-4611-ABEA-4835BE822A4B.JPG'),
-    require('./Images/The Office/E04FF4FA-1ECE-4B26-8685-E0D8532F92E1.JPG'),
+    process.env.PUBLIC_URL +
+      '/Images/The Office/5E89C17A-C97C-4ABC-B383-5BEB423550FB.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/The Office/B15E082D-C93A-47D2-A96D-D9E6C4E62294.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/The Office/DDF2B544-5DBE-4611-ABEA-4835BE822A4B.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/The Office/E04FF4FA-1ECE-4B26-8685-E0D8532F92E1.JPG',
   ],
   tiied: [
-    require('./Images/Tiiied/Tiiied (1).jpg'),
-    require('./Images/Tiiied/Tiiied (2).jpg'),
-    require('./Images/Tiiied/Tiiied (3).jpg'),
-    require('./Images/Tiiied/Tiiied (4).jpg'),
-    require('./Images/Tiiied/Tiiied (5).jpg'),
-    require('./Images/Tiiied/Tiiied (6).jpg'),
-    require('./Images/Tiiied/Tiiied (7).jpg'),
-    require('./Images/Tiiied/Tiiied (8).jpg'),
-    require('./Images/Tiiied/Tiiied (9).jpg'),
-    require('./Images/Tiiied/Tiiied (10).jpg'),
-    require('./Images/Tiiied/Tiiied (11).jpg'),
-    require('./Images/Tiiied/Tiiied (12).jpg'),
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (1).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (2).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (3).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (4).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (5).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (6).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (7).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (8).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (9).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (10).jpg',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (11).png',
+    process.env.PUBLIC_URL + '/Images/Tiiied/Tiiied (12).jpg',
   ],
   overtime: [
-    require('./Images/OvertimeFits/OT (2).jpg'),
-    require('./Images/OvertimeFits/OT (3).jpg'),
-    require('./Images/OvertimeFits/OT (1).jpg'),
+    process.env.PUBLIC_URL + '/Images/OvertimeFits/OT (2).jpg',
+    process.env.PUBLIC_URL + '/Images/OvertimeFits/OT (3).jpg',
+    process.env.PUBLIC_URL + '/Images/OvertimeFits/OT (1).jpg',
   ],
   no_remorse: [
-    require('./Images/no_remorse/no remorse (1).jpg'),
-    require('./Images/no_remorse/no remorse (2).jpg'),
-    require('./Images/no_remorse/no remorse (3).jpg'),
-    require('./Images/no_remorse/no remorse (4).jpg'),
-    require('./Images/no_remorse/no remorse (5).jpg'),
-    require('./Images/no_remorse/no remorse (6).jpg'),
-    require('./Images/no_remorse/no remorse (7).jpg'),
-    require('./Images/no_remorse/no remorse (8).jpg'),
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (1).jpg',
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (2).jpg',
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (3).jpg',
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (4).jpg',
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (5).jpg',
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (6).jpg',
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (7).jpg',
+    process.env.PUBLIC_URL + '/Images/no_remorse/no remorse (8).jpg',
   ],
   dominatrix: [
-    require('./Images/dominatrix/nr1.jpg'),
-    require('./Images/dominatrix/nr2.jpg'),
-    require('./Images/dominatrix/nr3.jpg'),
-    require('./Images/dominatrix/nr4.jpg'),
-    require('./Images/dominatrix/nr5.jpg'),
+    process.env.PUBLIC_URL + '/Images/dominatrix/nr1.jpg',
+    process.env.PUBLIC_URL + '/Images/dominatrix/nr2.jpg',
+    process.env.PUBLIC_URL + '/Images/dominatrix/nr3.jpg',
+    process.env.PUBLIC_URL + '/Images/dominatrix/nr4.jpg',
+    process.env.PUBLIC_URL + '/Images/dominatrix/nr5.jpg',
   ],
   colors: [
-    require('./Images/ColorsProject_Indigo/Colors-Indigo-Cole-4.png'),
-    require('./Images/ColorsProject_Indigo/Colors-Indigo-Cole-14.png'),
-    require('./Images/ColorsProject_Indigo/Colors-Indigo-Cole-16.png'),
-    require('./Images/ColorsProject_Indigo/Colors-Indigo-Cole-23.png'),
+    process.env.PUBLIC_URL +
+      '/Images/ColorsProject_Indigo/Colors-Indigo-Cole-4.png',
+    process.env.PUBLIC_URL +
+      '/Images/ColorsProject_Indigo/Colors-Indigo-Cole-14.png',
+    process.env.PUBLIC_URL +
+      '/Images/ColorsProject_Indigo/Colors-Indigo-Cole-16.png',
+    process.env.PUBLIC_URL +
+      '/Images/ColorsProject_Indigo/Colors-Indigo-Cole-23.png',
   ],
   colorsOfLux: [
-    require('./Images/ColorsOfLux/mail 2.png'),
-    require('./Images/ColorsOfLux/mail.png'),
+    process.env.PUBLIC_URL + '/Images/ColorsOfLux/mail 2.png',
+    process.env.PUBLIC_URL + '/Images/ColorsOfLux/mail.png',
   ],
   show_must_go_on: [
-    require('./Images/Show Must Go On/Show Must Go On (1).jpg'),
-    require('./Images/Show Must Go On/Show Must Go On (2).jpg'),
-    require('./Images/Show Must Go On/Show Must Go On (3).jpg'),
-    require('./Images/Show Must Go On/Show Must Go On (4).jpg'),
+    process.env.PUBLIC_URL + '/Images/Show Must Go On/Show Must Go On (1).jpg',
+    process.env.PUBLIC_URL + '/Images/Show Must Go On/Show Must Go On (2).jpg',
+    process.env.PUBLIC_URL + '/Images/Show Must Go On/Show Must Go On (3).jpg',
+    process.env.PUBLIC_URL + '/Images/Show Must Go On/Show Must Go On (4).jpg',
   ],
   grounded: [
-    require('./Images/Grounded/Grounded (1).jpg'),
-    require('./Images/Grounded/Grounded (2).jpg'),
-    require('./Images/Grounded/Grounded (3).jpg'),
-    require('./Images/Grounded/Grounded (4).jpg'),
-    require('./Images/Grounded/Grounded (5).jpg'),
-    require('./Images/Grounded/Grounded (6).jpg'),
+    process.env.PUBLIC_URL + '/Images/Grounded/Grounded (1).jpg',
+    process.env.PUBLIC_URL + '/Images/Grounded/Grounded (2).jpg',
+    process.env.PUBLIC_URL + '/Images/Grounded/Grounded (3).jpg',
+    process.env.PUBLIC_URL + '/Images/Grounded/Grounded (4).jpg',
+    process.env.PUBLIC_URL + '/Images/Grounded/Grounded (5).jpg',
+    process.env.PUBLIC_URL + '/Images/Grounded/Grounded (6).jpg',
   ],
   hold_on_me: [
-    require('./Images/Hold_on_me/hom-1.jpg'),
-    require('./Images/Hold_on_me/hom-2.jpg'),
-    require('./Images/Hold_on_me/hom-3.jpg'),
+    process.env.PUBLIC_URL + '/Images/Hold_on_me/hom-1.jpg',
+    process.env.PUBLIC_URL + '/Images/Hold_on_me/hom-2.jpg',
+    process.env.PUBLIC_URL + '/Images/Hold_on_me/hom-3.jpg',
   ],
   cage: [
-    require('./Images/Cage/Cage (1).jpg'),
-    require('./Images/Cage/Cage (2).jpg'),
-    require('./Images/Cage/Cage (3).jpg'),
-    require('./Images/Cage/Cage (4).jpg'),
+    process.env.PUBLIC_URL + '/Images/Cage/Cage (1).jpg',
+    process.env.PUBLIC_URL + '/Images/Cage/Cage (2).jpg',
+    process.env.PUBLIC_URL + '/Images/Cage/Cage (3).jpg',
+    process.env.PUBLIC_URL + '/Images/Cage/Cage (4).jpg',
   ],
   yasapothecary: [
-    require('./Images/Yasapothecary/27B0A6F3-C602-489D-BBDD-704DF3735DD4.JPG'),
-    require('./Images/Yasapothecary/683D448C-6D32-4AB5-9C0C-9B754102C70B.JPG'),
-    require('./Images/Yasapothecary/21605079-C338-47F2-8E5C-F746D16E7FD7.JPG'),
-    require('./Images/Yasapothecary/D2E40559-1946-40FD-B8E8-A2A6BB84F7D4.JPG'),
-    require('./Images/Yasapothecary/D072EF99-B5C5-4719-9A9F-72F166304576.JPG'),
+    process.env.PUBLIC_URL +
+      '/Images/Yasapothecary/27B0A6F3-C602-489D-BBDD-704DF3735DD4.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/Yasapothecary/683D448C-6D32-4AB5-9C0C-9B754102C70B.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/Yasapothecary/21605079-C338-47F2-8E5C-F746D16E7FD7.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/Yasapothecary/D2E40559-1946-40FD-B8E8-A2A6BB84F7D4.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/Yasapothecary/D072EF99-B5C5-4719-9A9F-72F166304576.JPG',
   ],
   fresh2death: [
-    require('./Images/Fresh2Death by Fourthebr4nd/3CD327C1-6A4A-4671-BB1A-43410BF8B565.JPG'),
-    require('./Images/Fresh2Death by Fourthebr4nd/AfterlightImage 2.jpeg'),
-    require('./Images/Fresh2Death by Fourthebr4nd/AfterlightImage.jpeg'),
-    require('./Images/Fresh2Death by Fourthebr4nd/D8D231D7-FFD2-4640-9DF7-C1A663685613.JPG'),
+    process.env.PUBLIC_URL +
+      '/Images/Fresh2Death by Fourthebr4nd/3CD327C1-6A4A-4671-BB1A-43410BF8B565.JPG',
+    process.env.PUBLIC_URL +
+      '/Images/Fresh2Death by Fourthebr4nd/AfterlightImage 2.jpeg',
+    process.env.PUBLIC_URL +
+      '/Images/Fresh2Death by Fourthebr4nd/AfterlightImage.jpeg',
+    process.env.PUBLIC_URL +
+      '/Images/Fresh2Death by Fourthebr4nd/D8D231D7-FFD2-4640-9DF7-C1A663685613.JPG',
   ],
 };
 
